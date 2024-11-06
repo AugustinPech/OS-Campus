98c5738c6974e93f7f5c933ec986e0fca856b4c2
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-06 11:45:01 +0100

    separated creation from deletion

--- Windows/deleteStructure.ps1
+++ Windows/deleteStructure.ps1
@@ -1,5 +1,9 @@
 $list=Get-ADOrganizationalUnit -Filter 'Name -like "*"' | Format-Table Name -A
+$toRemove= "Sites" , "T0" , "T1" , "T2"
 foreach ($item in $list) {
-    Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false -Identity $item
-    Remove-ADOrganizationalUnit -Identity $item -Confirm:$false
+    if ($toRemove -contains $item) {
+        Write-Host "Removing $item"
+        Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false -Identity $item
+        Remove-ADOrganizationalUnit -Identity $item -Confirm:$false
+    }
 }
