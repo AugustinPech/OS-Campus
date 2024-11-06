d666993ccfac5251d3bc3548602910c94a5b53b0
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-06 14:53:35 +0100

    tetsing a new user name format

--- Windows/AD/Users and groups/createUsersAndGroups.ps1
+++ Windows/AD/Users and groups/createUsersAndGroups.ps1
@@ -1,35 +1,33 @@
 $user1Attributes = @{
     Name="User1"
     Role="User"
-    "Surname"="One"
-    "Mail"="one@devops.forest"
+    Mail="one@devops.forest"
     Enabled=$true
     AccountPassword=(Read-Host -AsSecureString "Enter password")
 }
 $devAttributes = @{
     Name="Developer"
     Role="Developer"
-    "Surname"="Two"
-    "Mail"="dev@devops.forest"
+    Mail="dev@devops.forest"
     Enabled=$true
     AccountPassword=(Read-Host -AsSecureString "Enter password")
 }
 $adminAttributes = @{
     Name="Admin"
     Role="Admin"
-    "Surname"="Admin"
-    "Mail"="support@devops.forest"
+    Mail="support@devops.forest"
     Enabled=$true
     AccountPassword=(Read-Host -AsSecureString "Enter password")
 }
 
 $users= $user1Attributes, $devAttributes , $adminAttributes
 
-New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Administrators"
-New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Developers"
-New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Users"
+# New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+# New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Developers"
+# New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Users"
 
 foreach ($user in $users) {
+    echo $user.name
     New-ADUser $user
     if ($user.Role -eq "Admin") {
         Add-ADGroupMember -Identity Admins -Members $user.Name
