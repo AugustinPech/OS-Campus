57efa26e02f4c1632b2cb37d552817e7af802d4c
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-06 14:03:25 +0100

    fix : switched selection field for distinguishedname

--- Windows/deleteStructure.ps1
+++ Windows/deleteStructure.ps1
@@ -1,9 +1,9 @@
 $list=Get-ADOrganizationalUnit -Filter 'Name -like "*"'
 $toRemove= "Sites" , "T0" , "T1" , "T2"
 foreach ($item in $list) {
-    if ($toRemove -contains $item.Name) {
-        Write-Host "Removing $item.Name"
-        Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false -Identity $item.Name
-        Remove-ADOrganizationalUnit -Identity $item.Name -Confirm:$false -Recursive
+    if ($toRemove -contains $item.DistinguishedName) {
+        Write-Host "Removing " $item.DistinguishedName
+        Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false -Identity $item.DistinguishedName
+        Remove-ADOrganizationalUnit -Identity $item.DistinguishedName -Confirm:$false -Recursive
     }
 }
