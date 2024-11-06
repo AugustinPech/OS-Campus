0844ce8b6945bf64422f29293093c3d76fec2545
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-06 16:02:15 +0100

    Delete users script is up

--- Windows/AD/Users and groups/deleteUsersAndGroups.ps1
+++ Windows/AD/Users and groups/deleteUsersAndGroups.ps1
@@ -18,14 +18,8 @@ $users= $user1Attributes, $devAttributes , $adminAttributes
 # New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "CN=Users,DC=devops,DC=forest" -Description "Members of this group are Users"
 
 foreach ($user in $users) {
-    echo $user.name
-    New-ADUser -Name $user.Name
-    if ($user.Role -eq "Admin") {
-        Add-ADGroupMember -Identity Admins -Members $user.Name
-    } elseif ($user.Role -eq "Developer") {
-        Add-ADGroupMember -Identity Developers -Members $user.Name
-    } else {
-        Add-ADGroupMember -Identity Users -Members $user.Name
-    }
+
+    Remove-ADUser -Identity $user.Name -Confirm:$false
+    
 }
 
