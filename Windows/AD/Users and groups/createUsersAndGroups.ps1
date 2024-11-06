2a2906fc4c6c5b73f8195eef70c0fa45c56f4194
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-06 16:44:51 +0100

    created users and groups for each tier

--- Windows/AD/Users and groups/createUsersAndGroups.ps1
+++ Windows/AD/Users and groups/createUsersAndGroups.ps1
@@ -1,14 +1,85 @@
 # Définir les groupes
-# New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Administrators"
-# New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Developers"
-# New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Users"
+New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "OU=Groups,OU=T0,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "OU=Groups,OU=T0,DC=devops,DC=forest" -Description "Members of this group are Developers"
+New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "OU=Groups,OU=T0,DC=devops,DC=forest" -Description "Members of this group are Users"
 
-# Créer les utilisateurs pour Site1
+New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "OU=Groups,OU=T1,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "OU=Groups,OU=T1,DC=devops,DC=forest" -Description "Members of this group are Developers"
+New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "OU=Groups,OU=T1,DC=devops,DC=forest" -Description "Members of this group are Users"
+
+New-ADGroup -Name "Admins" -SamAccountName Admins -GroupCategory Security -GroupScope Global -DisplayName "Administrators" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+New-ADGroup -Name "Developers" -SamAccountName Developers -GroupCategory Security -GroupScope Global -DisplayName "Developers" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Developers"
+New-ADGroup -Name "Users" -SamAccountName Users -GroupCategory Security -GroupScope Global -DisplayName "Users" -Path "OU=Groups,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest" -Description "Members of this group are Users"
+
+# Créer les utilisateurs pour T0
 $password = (ConvertTo-SecureString -AsPlainText "P@ssword123" -Force)
-
-for ($i = 1; $i -le 20; $i++) {
+for ($i = 1; $i -le 10; $i++) {
     # Définir les attributs des utilisateurs
-    $username = "User$i"
+    $username = "User0$i"
+    $role = if ($i -le 2) { "Admin" } elseif ($i -le 5) { "Developer" } else { "User" }
+    $userAttributes = @{
+        Name = $username
+        GivenName = "FirstName$i"
+        Surname = "LastName$i"
+        SamAccountName = $username
+        UserPrincipalName = "$username@devops.forest"
+        Path = "OU=Users,OU=T0,DC=devops,DC=forest"
+        AccountPassword = $password
+        Enabled = $true
+        PasswordNeverExpires = $false
+        ChangePasswordAtLogon = $true
+    }
+
+    # Créer l'utilisateur
+    New-ADUser @userAttributes
+
+    # Ajouter l'utilisateur au groupe correspondant
+    switch ($role) {
+        "Admin" { Add-ADGroupMember -Identity "Admins" -Members $username }
+        "Developer" { Add-ADGroupMember -Identity "Developers" -Members $username }
+        "User" { Add-ADGroupMember -Identity "Users" -Members $username }
+    }
+
+    Write-Host "User $username with role $role created and added to the appropriate group."
+}
+
+# Créer les utilisateurs pour T1
+$password = (ConvertTo-SecureString -AsPlainText "P@ssword123" -Force)
+for ($i = 1; $i -le 10; $i++) {
+    # Définir les attributs des utilisateurs
+    $username = "User1$i"
+    $role = if ($i -le 2) { "Admin" } elseif ($i -le 5) { "Developer" } else { "User" }
+    $userAttributes = @{
+        Name = $username
+        GivenName = "FirstName$i"
+        Surname = "LastName$i"
+        SamAccountName = $username
+        UserPrincipalName = "$username@devops.forest"
+        Path = "OU=Users,OU=T1,DC=devops,DC=forest"
+        AccountPassword = $password
+        Enabled = $true
+        PasswordNeverExpires = $false
+        ChangePasswordAtLogon = $true
+    }
+
+    # Créer l'utilisateur
+    New-ADUser @userAttributes
+
+    # Ajouter l'utilisateur au groupe correspondant
+    switch ($role) {
+        "Admin" { Add-ADGroupMember -Identity "Admins" -Members $username }
+        "Developer" { Add-ADGroupMember -Identity "Developers" -Members $username }
+        "User" { Add-ADGroupMember -Identity "Users" -Members $username }
+    }
+
+    Write-Host "User $username with role $role created and added to the appropriate group."
+}
+
+# Créer les utilisateurs pour T2/Sites/Site1
+$password = (ConvertTo-SecureString -AsPlainText "P@ssword123" -Force)
+for ($i = 1; $i -le 10; $i++) {
+    # Définir les attributs des utilisateurs
+    $username = "User2$i"
     $role = if ($i -le 2) { "Admin" } elseif ($i -le 5) { "Developer" } else { "User" }
     $userAttributes = @{
         Name = $username
--- Windows/AD/Users and groups/deleteUsersAndGroups.ps1
+++ Windows/AD/Users and groups/deleteUsersAndGroups.ps1
@@ -2,7 +2,7 @@
 function CleanUp {
     Write-Host "Cleaning up users and groups created in Site1..."
 
-    $list = Get-ADUser -Filter 'Name -like "User*"' -SearchBase "OU=Users,OU=Site1,OU=Sites,OU=T2,DC=devops,DC=forest"
+    $list = Get-ADUser -Filter 'Name -like "User[0-9]*"'
     
     # Suppression des utilisateurs
     foreach ($username in $list) {
