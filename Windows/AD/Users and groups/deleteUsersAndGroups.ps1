faf44d0f04b2900a9350bfb2ef9db351fd53cd36
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-07 13:11:01 +0100

    fixing a group name conflict0

--- Windows/AD/Users and groups/deleteUsersAndGroups.ps1
+++ Windows/AD/Users and groups/deleteUsersAndGroups.ps1
@@ -8,7 +8,7 @@ function CleanUp {
         Remove-ADUser -Identity $username -Confirm:$false
         Write-Host "User $username has been removed."
     }
-    $groups = Get-ADGroup -Filter 'Name -like "T0*" -or Name -like "T1*" -or Name -like "T2*"'
+    $groups = Get-ADGroup -Filter 'Name -like "T0*" -or Name -like "T1*" -or Name -like "T2*" -or Name -like "Site1*" -or Name -like "Site2*" -or Name -like "Site3*"'
     # Suppression des groupes
     foreach ($group in $groups) {
         Remove-ADGroup -Identity $group.DistinguishedName -Confirm:$false
