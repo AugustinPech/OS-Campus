7e06680c25cf795e30c8511983d4143cd6ec8b84
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-11-07 12:21:37 +0100

    fixing a group name conflict

--- Windows/AD/Users and groups/createUsersAndGroups.ps1
+++ Windows/AD/Users and groups/createUsersAndGroups.ps1
@@ -2,10 +2,13 @@ $tiers = "T0", "T1", "T2"
 $sites= "Site1", "Site2", "Site3"
 # Définir les groupes
 foreach ($tier in $tiers) {
-    $admins=New-ADGroup -Name "$tier Admins" -SamAccountName "${tier}Admins" -GroupCategory Security -GroupScope Global -DisplayName "$tier Administrators" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+    New-ADGroup -Name "$tier Admins" -SamAccountName "${tier}Admins" -GroupCategory Security -GroupScope Global -DisplayName "$tier Administrators" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Administrators"
+    $admins=Get-ADGroup -Filter { Name -eq "$tier Admins" }
     echo "-- $admins"
-    $devs=New-ADGroup -Name "$tier Developers" -SamAccountName "${tier}Developers" -GroupCategory Security -GroupScope Global -DisplayName "$tier Developers" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Developers"
-    $users=New-ADGroup -Name "$tier Users" -SamAccountName "${tier}Users" -GroupCategory Security -GroupScope Global -DisplayName "$tier Users" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Users"
+    New-ADGroup -Name "$tier Developers" -SamAccountName "${tier}Developers" -GroupCategory Security -GroupScope Global -DisplayName "$tier Developers" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Developers"
+    $devs=Get-ADGroup -Filter { Name -eq "$tier Developers" }
+    New-ADGroup -Name "$tier Users" -SamAccountName "${tier}Users" -GroupCategory Security -GroupScope Global -DisplayName "$tier Users" -Path "OU=Groups,OU=$tier,DC=devops,DC=forest" -Description "Members of this group are Users"
+    $users=Get-ADGroup -Filter { Name -eq "$tier Users" }
     $password = (ConvertTo-SecureString -AsPlainText "P@ssword123" -Force)
     # Créer les utilisateurs pour $tier
     for ($i = 1; $i -le 10; $i++) {
